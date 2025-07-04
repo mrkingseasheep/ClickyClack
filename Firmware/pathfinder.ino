0b98b5b9ba5c1b452604e6e34189f90300154205
Xinyang Wang <136667147+mrkingseasheep@users.noreply.github.com>
2025-07-04 19:41:05 -0400

    Init

---
+++ Firmware/pathfinder.ino
@@ -0,0 +1,35 @@
+void setup() {
+
+pinMode(BUT1, INPUT);
+
+pinMode(BUT2, INPUT);
+
+pinMode(BUT3, INPUT);
+
+
+pinMode(LED1, OUTPUT);
+
+pinMode(LED2, OUTPUT);
+
+pinMode(LED3, OUTPUT);
+
+
+long startTime = millis();
+
+}
+
+void loop() {
+
+long but1time;
+long but2time;
+
+long but3time = 0;
+}
+
+aoesnitoheitnh
+
+ahaaaaaaaaaaoeusntahoeistnoeahiesantihoesntntjahksntoehuoaesnethueasntoehuesnutheousntantehusaontehuoasntheueosnthusntjhkoeusnaothueaosntuhoeusnthoeusntaohuoeasntuhostnjhkaoeusaonteuhesnuthoeoeusntoehuosntuhaoesnthoeuasnteuhaoesnthioesantihoesunthasoeuhoeasntuheausnetouheusntehasoentuheoauosntehueosuntoehousentuhoaesunthuaesnuteohutnshatuhausntoehusenothueasnthAasntehuoesntuhth
+
+long long long long long
+oeusoeatuhsntheuntheounth ------------------------nthnthnththththththththththtthaeauoeuoeuoeuhdhhthuieihhthththt
+eouaoentsuhoeusnth
